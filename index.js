21e266509062c357754fe37f4be60b0731186334
MRUTYUNJAY KUMAR <mrutyunjay.k@iserveu.in>
2022-04-11 16:10:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 8917;
+
+
+
+const path = require('path')
+app.use(express.static(path.join(__dirname + '/public')));
+
+/*app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});*/
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
